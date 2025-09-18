56f15ce744e28f45e8fe3592b1fc77ffbd3b2a66
Michael <stevenex13@gmail.com>
2025-09-18 17:57:26 -0500

    cors

--- server.js
+++ server.js
@@ -10,7 +10,11 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://convetidor-front.vercel.app",
+  })
+);
 app.use(express.json());
 
 // Routes
